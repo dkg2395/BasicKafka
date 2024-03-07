175d9a9c890b8e542d0b54705045152ebbc2da4c
DevOpsFse <Fse22022@gmail.com>
2024-03-07 20:23:32 +0530

    kafka project producer & consumer

---
+++ springboot-kafka-tutorial/src/test/java/com/durgeshtest/SpringbootKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durgeshtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
